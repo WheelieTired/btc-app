56f567765b6ac7ce82403dba9c6ce94e8914e0a7
Sam Falconer <sam@samfalconer.com>
2017-02-04 14:21:30 -0500

    Jump through the immutable/mutable hoops for the tracks reducer.

--- src/js/store.js
+++ src/js/store.js
@@ -9,8 +9,9 @@ import filters from './reducers/filter';
 import account from './reducers/account';
 import drawer from './reducers/btc-drawer';
 
+import { fromJS } from 'immutable';
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
-import { persistStore, autoRehydrate } from 'redux-persist'
+import { persistStore, autoRehydrate, createTransform } from 'redux-persist'
 import thunk from 'redux-thunk';
 
 const devTools = typeof window === 'object' && typeof window.devToolsExtension !== 'undefined' ? window.devToolsExtension() : f => f;
@@ -31,6 +32,22 @@ const theStore = compose.apply( null, args )( createStore )( combineReducers( {
   account,
 drawer } ) );
 
-persistStore(theStore);
+function specialSerializeMakeMutable(inboundState, key) {
+	return inboundState.toJS();
+}
+
+function specialDeserializeMakeImmutable(outboundState, key) {
+	return(fromJS(outboundState));
+}
+
+// The tracks reducer wraps its state in the immutable library's imutableness.
+// We need to add these extra steps on serialize and deserialize.
+let immutableTransformer = createTransform(
+  (inboundState, key) => specialSerializeMakeMutable(inboundState, key),
+  (outboundState, key) => specialDeserializeMakeImmutable(outboundState, key),
+  {whitelist: ['tracks']}
+);
+
+persistStore(theStore, {transforms: [immutableTransformer]});
 
 export default theStore;
