c181f0f5d1e5790ca00f00cb56ef21bb5760e9ba
Austin Klein <amk5401@rit.edu>
2017-02-07 15:56:12 -0500

    Added json for onboarding panel text

--- src/js/containers/onboarding.js
+++ src/js/containers/onboarding.js
@@ -13,26 +13,42 @@ import { setDrawer } from '../reducers/btc-drawer';
 export class OnboardingPage extends Component {
   constructor(props) {
       super(props);
+
+      var data = require('../../json/onboardingData.json');
+      this.totalItems = data.length;
+      this.onboardingText = [];
+      for (var i = 0; i < data.length; i++) {
+        var obj = data[i];
+        this.onboardingText.push(obj.text);
+      }
+
       this.state = {
-        activePage: 1
+        activePage: 1,
+        text: ""
       };
   }
 
   handlePageChange(pageNumber) {
     //console.log(`active page is ${pageNumber}`);
-    this.setState({activePage: pageNumber});
+    this.setState({activePage: pageNumber, text: this.onboardingText[pageNumber-1]});
   }
 
   render() {
-    const image = 'img/onboarding'+ this.state.activePage + '.png';
+    var image = 'img/onboarding'+ this.state.activePage + '.png';
     return (
       <div style={{height:'100%'}}>
-        <img src={image} style={{zIndex:'1200'}} />
-        <div style={{zIndex:'1300', display: 'flex', alignItems:'center', justifyContent:'center', position: 'fixed', bottom:'75px'}}>
+        <img src={image} style={{zIndex:'1200', boxSizing:'border-box', outline:'solid 75px rgb(0, 188, 212)'}} />
+        <p style={{
+          zIndex:'1300', display: 'flex', alignItems:'center', justifyContent:'center',
+          position:'fixed', bottom:'125px', backgroundColor:'rgb(0, 188, 212)', color:'white', textAlign:'center'
+        }}>
+        {this.state.text}
+        </p>
+        <div style={{zIndex:'1300', display: 'flex', alignItems:'center', justifyContent:'center', position:'fixed', bottom:'75px'}}>
           <Pagination
             activePage={this.state.activePage}
             itemsCountPerPage={1}
-            totalItemsCount={5}
+            totalItemsCount={this.totalItems}
             pageRangeDisplayed={5}
             onChange={this.handlePageChange.bind(this)}
           />
