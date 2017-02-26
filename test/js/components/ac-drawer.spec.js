8288e324ab9afca6aaa4652ab93cece7f5a294ef
Jeremy <jsf6410@g.rit.edu>
2017-02-26 17:50:54 -0500

    Added some pretty basic tests to the drawer and onboarding.

--- test/js/components/ac-drawer.spec.js
+++ test/js/components/ac-drawer.spec.js
@@ -19,6 +19,30 @@ describe( '<Drawer />', function() {
 
     expect( instance.state.open ).to.be.false;
   } );
+  
+  it ( 'should open when clicked', function() {
+    const tree = sd.shallowRender( (
+	      <BtcDrawer drawer="New Page"
+	        login={ { loggedIn: false } } />
+	      ) );
+	
+	    const instance = tree.getMountedInstance();
+	    instance.showNav();
+    expect( instance.state.open ).to.be.true;
+  } );
+  
+  it ( 'should close when clicked if already open', function() {
+	    const tree = sd.shallowRender( (
+		      <BtcDrawer drawer="New Page"
+		        login={ { loggedIn: false } } />
+		      ) );
+		
+		    const instance = tree.getMountedInstance();
+		    instance.open={ true };
+		    instance.hideNav();
+	    expect( instance.state.open ).to.be.false;
+	  } );
+  
   it( 'should set the title from the drawer prop', function() {
     const tree = sd.shallowRender( (
       <BtcDrawer drawer="New Page"
