32fa1669fc6c16ed09f4c2cc6bf5ad539d87d4ff
Sam Falconer <sam@samfalconer.com>
2017-03-04 18:58:07 -0500

    Just put this back in master. I was going to merge it immediately anyway and git is being a pain.
    Revert "Revert "Update all Node and Cordova dependencies.""
    
    This reverts commit 4e5d316a0d8744abfe06670d44e7bf5e7b4d88fb.

--- src/js/components/map-buttons.js
+++ src/js/components/map-buttons.js
@@ -3,6 +3,8 @@ import React, { Component, PropTypes } from 'react';
 import { FloatingActionButton, FontIcon } from 'material-ui';
 /*eslint-enable no-unused-vars*/
 
+import history from '../history';
+
 // MapButtons takes in props for buttons, in which buttons should be an array of
 // button objects, like so:
 /*
@@ -14,8 +16,9 @@ export class MapButtons extends Component {
       return (
         <FloatingActionButton key={ button.page }
           mini={ true }
+          className="mapButtons"
           style={ { position: 'fixed', top: `${82 + 55 * index}px`, right: '10px' } }
-          onTouchTap={ ( ) => this.props.history.push( button.page ) }>
+          onTouchTap={ ( ) => history.push( button.page ) }>
           <FontIcon className="material-icons">
             { button.icon }
           </FontIcon>
