67e2f655aee78a6de5be3a4c6dd467da02e24b65
Asma A Sattar <aas3799@rit.edu>
2017-03-07 21:12:52 -0500

    fixed upload photo button

--- src/js/components/wizard/wizard-page.js
+++ src/js/components/wizard/wizard-page.js
@@ -225,7 +225,7 @@ export class WizardPage extends Component {
 
             let context = canvas.getContext('2d');
             context.drawImage(loadedCoverBlob, 0, 0, newWidth, newHeight);
-            let resizedDataUrl = canvas.toDataURL('image/jpg');
+            let resizedDataUrl = canvas.toDataURL('image/jpeg');
 
             let resizedCoverBlob = dataURLToBlob(resizedDataUrl);
 
@@ -237,10 +237,22 @@ export class WizardPage extends Component {
       }
     }, err => {
       console.error( err );
-    }, {
-      sourceType: navigator.camera.PictureSourceType.PHOTOLIBRARY,
-      destinationType: navigator.camera.DestinationType.FILE_URI,
-      encodingType: navigator.camera.EncodingType.JPG
+    },{
+      // Some common settings are 20, 50, and 100
+      quality: 50,
+      destinationType: Camera.DestinationType.FILE_URI,
+
+      // In this app, dynamically set the picture source, Camera or photo gallery
+                                      
+      //then a dialog displays that allows users to select an existing image
+      sourceType: Camera.PictureSourceType.PHOTOLIBRARY, /* SAVEDPHOTOALBUM  both work*/
+      encodingType: Camera.EncodingType.JPG,
+      mediaType: Camera.MediaType.PICTURE,
+      allowEdit: true,
+      correctOrientation: true, //Corrects Android orientation quirks
+      //targetWidth: 512,
+      //targetHeight: 512,
+      cameraDirection: Camera.Direction.BACK
     } );
   }
 }
